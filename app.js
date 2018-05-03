f5299e21bfd32538a8bd70b129a1129df9cfb8ea
Robyn Navarro <rnavarro182@gmail.com>
2018-05-03 15:54:36 -0700

    JS progress

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 /* globals words */
-
-var answer ='';
+var guessedLetters = [];
+var answer = '';
 var blankSpaces = 0;
 
 var loadWord = function(){
-    
+
     var index = randomWord(words.length);
     answer = words[index];
     blankSpaces = answer.length;
 
     
-    };
+};
 
-    console.log(answer);
-    console.log(blankSpaces); //length of the answer - amount of letters
-    
+console.log(answer);
+console.log(blankSpaces); //length of the answer - amount of letters  
 
 
 function randomWord(max){
@@ -33,11 +32,20 @@ console.log(blankSpaces); //length of the answer - amount of letters
 
 
 //draw right amount of blank spaces
-for (i=1; i <= blankSpaces ; i++){
+for(i = 1; i <= blankSpaces ; i++){
     var blanks = document.getElementById('word-to-guess');
     blanks.innerHTML += '_ ';
 
 }
+
+function guessLetter(){
+    var letter = document.getElementById('letter');
+    console.log('the letter is', letter.value);
+    guessedLetters.push(letter.value);
+    console.log(guessedLetters);
+}
+
+console.log(letter);
 
 //display correct letters
 //display guessed letters
